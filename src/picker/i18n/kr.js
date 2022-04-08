dc295c8ca05b03d2944feaaf41d0d3d5ccb02192
miuju <75881943+miuju@users.noreply.github.com>
2022-04-08 10:21:35 +0900

    Create kr.js

---
+++ src/picker/i18n/kr.js
@@ -0,0 +1,34 @@
+export default {
+  categoriesLabel: '카테고리',
+  emojiUnsupportedMessage: '귀하의 브라우저는 컬러 이모티콘을 지원하지 않습니다.',
+  favoritesLabel: '즐겨찾기',
+  loadingMessage: '로딩 중…',
+  networkErrorMessage: '이모티콘을 로드할 수 없습니다.',
+  regionLabel: '이모티콘 선택기',
+  searchDescription: '검색 결과를 사용할 수 있는 경우 위 또는 아래를 눌러 선택하고 Enter를 눌러 선택합니다.',
+  searchLabel: '검색',
+  searchResultsLabel: '검색 결과',
+  skinToneDescription: '확장되면 위 또는 아래를 눌러 선택하고 Enter 키를 눌러 선택합니다.',
+  skinToneLabel: '피부색 선택(현재 {skinTone})',
+  skinTonesLabel: '피부색',
+  skinTones: [
+    '기본',
+    '빛',
+    '중간 빛',
+    '중간',
+    '중간 어두운',
+    '어두운'
+  ],
+  categories: {
+    custom: 'Custom',
+    'smileys-emotion': '스마일리와 이모티콘',
+    'people-body': '사람과 몸',
+    'animals-nature': '동물과 자연',
+    'food-drink': '음식과 물',
+    'travel-places': '여행과 장소',
+    activities: '활동',
+    objects: '물건들',
+    symbols: '기호',
+    flags: '깃발'
+  }
+}
